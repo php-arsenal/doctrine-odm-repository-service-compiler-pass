d156d1371c5fb36ac621e38864355d222391311b
Gyvastis <shibalba1@gmail.com>
2021-06-19 09:28:49 +0200

    init

---
+++ src/DocumentRepositoryAutoconfigureCompilerPass.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace PhpArsenal\DoctrineODMRepositoryServiceCompilerPass;
+
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\DocParser;
+use Doctrine\ODM\MongoDB\Mapping\Annotations\Document;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+class DocumentRepositoryAutoconfigureCompilerPass implements CompilerPassInterface
+{
+    private AnnotationReader $annotationReader;
+
+    public function __construct()
+    {
+        $this->annotationReader = new AnnotationReader(new DocParser());
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        foreach ($this->getFullyQualifiedDocumentClassNames($container) as $documentClass) {
+            if ($container->getReflectionClass($documentClass)->isAbstract()) {
+                continue;
+            }
+
+            /** @var Document $documentAnnotation */
+            $documentAnnotation = $this->annotationReader->getClassAnnotation($container->getReflectionClass($documentClass), Document::class);
+            if (!$documentAnnotation) {
+                continue;
+            }
+            $documentRepositoryClass = $documentAnnotation->repositoryClass;
+
+            if (!in_array($documentRepositoryClass, $container->getServiceIds()) && class_exists($documentRepositoryClass)) {
+                $this->addDocumentRepositoryService($container, $documentClass, $documentRepositoryClass);
+            }
+        }
+    }
+
+    private function getFullyQualifiedDocumentClassNames(ContainerBuilder $container): array
+    {
+        $doctrineConfig = array_merge(...$container->getExtensionConfig('doctrine_mongodb'));
+        $documentDirs = array_map(function (array $mappingConfig) use ($container) {
+            return $container->getParameterBag()->resolveValue($mappingConfig['dir']);
+        }, $doctrineConfig['document_managers']['default']['mappings']);
+
+        $documentClasses = [];
+        foreach ($documentDirs as $documentDir) {
+            $documentClasses = array_merge($documentClasses, FullyQualifiedClassNameReader::readAll($documentDir));
+        }
+
+        return $documentClasses;
+    }
+
+    private function addDocumentRepositoryService(ContainerBuilder $container, string $documentClass, string $documentRepositoryClass): void
+    {
+        $repositoryServiceDefinition = new Definition($documentRepositoryClass);
+        $repositoryServiceDefinition->setFactory([new Reference('doctrine_mongodb.odm.document_manager'), 'getRepository']);
+        $repositoryServiceDefinition->addArgument($documentClass);
+        $repositoryServiceDefinition->setAutowired(true);
+        $repositoryServiceDefinition->setAutoconfigured(true);
+
+        $container->setDefinition($documentRepositoryClass, $repositoryServiceDefinition);
+    }
+}
---
+++ src/FullyQualifiedClassNameReader.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace PhpArsenal\DoctrineODMRepositoryServiceCompilerPass;
+
+use Symfony\Component\Finder\Finder;
+
+class FullyQualifiedClassNameReader
+{
+    public static function readAll(string $sourceDir): array
+    {
+        $classNames = [];
+
+        foreach (static::getFilePaths($sourceDir) as $filePath) {
+            $fileContents = file_get_contents($filePath);
+            $classNames[] = static::getFileNamespace($fileContents) . '\\' . static::getFileClassName($fileContents);
+        }
+
+        return $classNames;
+    }
+
+    private static function getFileNamespace(string $fileContents): string
+    {
+        preg_match('/^namespace\s+([^;]+)\s+?;$/', $fileContents, $match);
+
+        return $match[1];
+    }
+
+    private static function getFileClassName(string $fileContents): string
+    {
+        preg_match('/^class\s+([^{]*)\s+?$/', $fileContents, $match);
+
+        return $match[1];
+    }
+
+    private static function getFilePaths(string $sourceDir): array
+    {
+        $filePaths = [];
+
+        foreach (Finder::create()->files()->in($sourceDir)->name('*.php') as $file) {
+            $filePaths[] = $file->getRealpath();
+        }
+
+        return $filePaths;
+    }
+}
